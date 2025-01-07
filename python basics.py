9d56cd65d8d1e534f7e91d92cb7af6a2c6a2638c
VKR2004 <vkrvkr2004@gmail.com>
2025-01-07 10:28:56 +0530

    Add files via upload

---
+++ python basics.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+a="machine learning"
+a[0:7]
+
+
+# In[7]:
+
+
+a[0:7:1]
+
+
+# In[8]:
+
+
+a[0:7:2]
+
+
+# In[9]:
+
+
+a[0:7:3]
+
+
+# In[12]:
+
+
+a[0:6:1]
+
+
+# In[13]:
+
+
+a='machine'
+a.swapcase()
+
+
+# In[ ]:
+
+
+
+
